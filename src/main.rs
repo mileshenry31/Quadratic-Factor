8da954ca08a934dd7bd804d841cdb1a153a97d4c
mileshenry31 <milesmhenry@icloud.com>
2022-07-22 08:42:06 -0700

    Added comments

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,21 @@ fn main() {
 fn formula(a: i32, b: i32, c: i32) -> (i32, i32) {
     // Get the top number for the diamond
     let ac = a * c;
+    // Variable that is the top number divided by the current `i` number
     let mut mul_num: i32;
+    // Start at one and check through every number up to the top one to find the multiples
     for i in 1..=ac {
+        // Find even divisibles
         if ac % i == 0 {
+            // Top number divided by current `i` number
             mul_num = ac / i;
+            // If it matches return the tuple that contains the two numbers
             if mul_num + i == b {
                return (mul_num, i); 
             }
         }
     }
+    // If it doesnt work return a tuple of zeros
     (0, 0)
 }
 
