75987d328a3413e4c9603654e638f213c13bedbe
mileshenry31 <milesmhenry@icloud.com>
2022-07-21 23:06:34 -0700

    Added formula function, works for only positive numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+
+
 fn main() {
-    println!("Hello, world!");
+   println!("{:?}", formula(12, -5, -2));
 }
+
+fn formula(a: i32, b: i32, c: i32) -> (i32, i32) {
+    // Get the top number for the diamond
+    let ac = a * c;
+    let mut mul_num: i32;
+    for i in 1..=ac {
+        if ac % i == 0 {
+            mul_num = ac / i;
+            if mul_num + i == b {
+               return (mul_num, i); 
+            }
+        }
+    }
+    (0, 0)
+}
+
+
