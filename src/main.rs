4f5eec9a116fd77ca5f8bc6863aba4ce12a6b383
mileshenry31 <milesmhenry@icloud.com>
2022-07-22 13:27:48 -0700

    Negative numbers work

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 
 fn main() {
-   println!("{:?}", formula(3, 10, 3));
+   println!("{:?}", formula(0, -5, -2)); // Should return (9, 1)
 }
 
 fn formula(a: i32, b: i32, c: i32) -> (i32, i32) {
@@ -10,17 +10,29 @@ fn formula(a: i32, b: i32, c: i32) -> (i32, i32) {
     // Variable that is the top number divided by the current `i` number
     let mut mul_num: i32;
     // Start at one and check through every number up to the top one to find the multiples
-    for i in 1..=ac {
-        // Find even divisibles
-        if ac % i == 0 {
-            // Top number divided by current `i` number
-            mul_num = ac / i;
-            // If it matches return the tuple that contains the two numbers
-            if mul_num + i == b {
-               return (mul_num, i); 
+    if ac > 0 {
+        for i in 1..=ac {
+            // Find even divisibles
+            if ac % i == 0 {
+                // Top number divided by current `i` number
+                mul_num = ac / i;
+                // If it matches return the tuple that contains the two numbers
+                if mul_num + i == b {
+                   return (mul_num, i); 
+                }
+            }
+        }
+    } else if ac < 0 {
+        for i in ac..=1 {
+            if ac % i == 0 {
+                mul_num = ac / i;
+                if mul_num * i == ac && mul_num + i == b {
+                    return (mul_num, i);
+                }
             }
         }
     }
+    
     // If it doesnt work return a tuple of zeros
     (0, 0)
 }
